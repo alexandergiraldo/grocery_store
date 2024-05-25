77b60ea0b5ccc2e2a8976e8d73362baf8d67e23b
Alexander Giraldo <algirald@cisco.com>
2024-05-25 16:23:46 -0500

    Initial structure of product and product sale

---
+++ domain/product_sale.rb
@@ -0,0 +1,11 @@
+class ProductSale
+
+  # pruduct: Product
+  # quantity: int
+  # sale_price: int
+  def initialize(product, quantity, sale_price)
+    @product = product
+    @quantity = quantity
+    @sale_price = sale_price
+  end
+end
